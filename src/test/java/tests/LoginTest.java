47af56458bd5523f5eff0a499b56576d464d323e
HeorhiKushnir <121432712+HeorhiKushnir@users.noreply.github.com>
2023-04-19 01:16:52 +0300

    Add files via upload

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,18 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void createNewProject() {
+        loginPage.open();
+        loginPage.login("reiybhujif@mail.ru", "4da8db88");
+        projectsPage.projectPageIsOpened();
+        projectsPage.createNewProject();
+        createNewProjectPage.fillInProjectFields("Test19" , "Test19");
+        createNewProjectPage.createProject();
+        settingsPage.openProjectSettings("TEST19");
+        settingsPage.validate("Test19", "TEST19");
+    }
+}
